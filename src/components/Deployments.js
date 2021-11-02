88e917be2f2d5b9f964c78aeed4994a2c62e4b22
acr-coder <acarerisa1@gmail.com>
2021-11-02 17:37:06 +0300

    second commit

--- src/components/Deployments.js
+++ src/components/Deployments.js
@@ -1,72 +1,82 @@
-import React from 'react'
-import './deployments.css'
+import React, { useState } from "react";
+import "./deployments.css";
+import DonutChart from "react-donut-chart";
 
 export const Deployments = () => {
-    return (
-        
+  const [serviceHealthSummary, setServiceHealthSummary] = useState({
+    passing:7,
+    atRisk:1,
+    failing:1
+  })
+  return (
     <div className="container">
-        <h2>Deployments</h2>
+      <h2>Deployments</h2>
       <div className="row row-cols-1 row-cols-sm-2 row-cols-md-5 g-3 ">
-      <div className="col">
+        <div className="col">
           <div className="card shadow-sm">
-           
-
             <div className="card-body">
-            <h5 className="title">Active Deployments</h5>
-           <h1>72</h1>
-              <h5 >0 inactive</h5>
+              <h5 className="title">Active Deployments</h5>
+              <h1>72</h1>
+              <h5>0 inactive</h5>
             </div>
           </div>
         </div>
         <div className="col">
           <div className="card shadow-sm">
-           
-
             <div className="card-body">
-            <h5 className="title">Active Deployments</h5>
-           <h1>1.40M <sup>(-3.4%)</sup></h1>
-              <h5 >0 inactive</h5>
+              <h5 className="title">Active Deployments</h5>
+              <h1>
+                1.40M <sup>(-3.4%)</sup>
+              </h1>
+              <h5>0 inactive</h5>
             </div>
           </div>
         </div>
         <div className="col">
           <div className="card shadow-sm">
-           
-
             <div className="card-body">
-            <h5>Service Health Summary</h5>
-           <h1>72</h1>
+              <h5>Service Health Summary</h5>
+              <DonutChart height={80} width={80} startAngle={270}
+              colors={['#1ABC9C', '#FFB806', '#F24C42',]}
+                data={[
+                  {
+                    label: `${serviceHealthSummary.passing} Passing `,
+                    value: serviceHealthSummary.passing,
+                    color:'#9c27b0'
+                  },
+                  {
+                    label: "At Risk",
+                    value: 1,
+                    
+                  },
+                  {
+                    label: "Failing",
+                    value: 1,
+                    
+                  },
+                ]}
+              />
             </div>
           </div>
         </div>
 
         <div className="col">
           <div className="card shadow-sm">
-           
-
             <div className="card-body">
-            <h5>Active Deployments</h5>
-           <h1>72</h1>
+              <h5>Active Deployments</h5>
+              <h1>72</h1>
             </div>
           </div>
         </div>
         <div className="col">
           <div className="card shadow-sm">
-           
-
             <div className="card-body">
-            <h5>Active Deployments</h5>
-           <h1>72</h1>
+              <h5>Active Deployments</h5>
+              <h1>72</h1>
             </div>
           </div>
         </div>
-        
-
-        
-        
-        
       </div>
     </div>
-  
-    )
-}
+  );
+};
